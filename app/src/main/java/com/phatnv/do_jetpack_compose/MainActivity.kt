b0e1493e0833e19640bd9fc979705af9bf25a866
phatnv <phat.nv@themansions.vn>
2023-10-19 14:45:26 +0700

    Test text

--- app/src/main/java/com/phatnv/do_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phatnv/do_jetpack_compose/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.phatnv.do_jetpack_compose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.phatnv.do_jetpack_compose.ui.theme.DojetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,21 +38,93 @@ class MainActivity : ComponentActivity() {
             DojetpackcomposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TextExample()
                 }
             }
         }
     }
 }
 
+fun Context.showToast(message: String, length: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(this, message, length).show()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextExample() {
+    /**
+     * https://github.com/Foso/Jetpack-Compose-Playground/blob/master/app/src/main/java/de/jensklingenberg/jetpackcomposeplayground/mysamples/github/foundation/TextExample.kt
+     * https://dmytroshuba.com/blog/complete-guide-to-rich-text-in-jetpack-compose/
+     * https://developer.android.com/jetpack/compose/text?hl=vi
+     * */
+    val context = LocalContext.current
+    val offset = Offset(5.0f, 10.0f)
+    Column(modifier = Modifier.padding(10.dp)) {
+        Text("Just Text")
+        Text("Text with cursive font", style = TextStyle(fontFamily = FontFamily.Cursive))
+        Text(
+            text = "Text with LineThrough",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(
+            text = "Text with underline",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        // Text combine, clickable
+        Text(text = "Text with underline, line through and bold", style = TextStyle(
+            textDecoration = TextDecoration.combine(
+                listOf(
+                    TextDecoration.Underline,
+                    TextDecoration.LineThrough,
+                )
+            ),
+            fontWeight = FontWeight.Bold,
+        ), modifier = Modifier.clickable {
+            // By default:
+            // Toast.makeText(context, "Hello World!", Toast.LENGTH_SHORT).show();
+            context.showToast("Hello World!");
+        })
+        // Text Overflow
+        Text(
+            text = "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+        )
+        // AnnotatedString/RichText
+        Text(
+            text = "Hello World!",
+            style = TextStyle(
+                fontSize = 24.sp,
+                shadow = Shadow(
+                    color = Color.Blue,
+                    offset = offset,
+                    blurRadius = 3f
+                )
+            )
+        )
+        // TextStyle, SpanStyle, ParagraphStyle
+        Text(
+            buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color.Blue)) {
+                    append("H")
+                }
+                append("ello ")
+
+                withStyle(style = SpanStyle(color = Color.Red, fontWeight = FontWeight.Bold)) {
+                    append("W")
+                }
+                append("orld")
+            }
+        )
+    }
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
