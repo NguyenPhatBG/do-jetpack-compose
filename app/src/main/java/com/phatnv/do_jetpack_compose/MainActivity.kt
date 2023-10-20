2067647f31708808b79371bb7cb994c427e6b521
phatnv <phat.nv@themansions.vn>
2023-10-20 15:01:27 +0700

    Update script text

--- app/src/main/java/com/phatnv/do_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phatnv/do_jetpack_compose/MainActivity.kt
@@ -2,29 +2,47 @@ package com.phatnv.do_jetpack_compose
 
 import android.content.Context
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
+import android.util.Patterns
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
@@ -35,6 +53,8 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.util.LinkifyCompat
 import com.phatnv.do_jetpack_compose.ui.theme.DojetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,6 +73,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Composable
+fun TextMeasure(text: String) {
+    Text(
+        text,
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.error, shape = CircleShape)
+            .badgeLayout(),
+        color = Color.White,
+    )
+}
+
+@Composable
+fun Hyperlink(modifier: Modifier = Modifier, text: String? = null, mask: Int = Linkify.WEB_URLS) {
+    val context = LocalContext.current
+    val customLinkifyTextView = remember {
+        TextView(context)
+    }
+    AndroidView(factory = { customLinkifyTextView }, modifier = modifier) { textView ->
+        // https://kotlinlang.org/docs/null-safety.html#what-s-next
+        textView.text = text ?: ""
+        LinkifyCompat.addLinks(textView, mask)
+        Linkify.addLinks(
+            textView,
+            Patterns.PHONE,
+            "tel:",
+            Linkify.sPhoneNumberMatchFilter,
+            Linkify.sPhoneNumberTransformFilter,
+        )
+        textView.movementMethod = LinkMovementMethod.getInstance()
+    }
+}
+
 // Made the following modifier using Modifier.layout
 fun Modifier.badgeLayout() = layout { measurable, constraints ->
     val placeable = measurable.measure(constraints)
@@ -66,19 +119,53 @@ fun Modifier.badgeLayout() = layout { measurable, constraints ->
     }
 }
 
-
 fun Context.showToast(message: String, length: Int = Toast.LENGTH_LONG) {
     Toast.makeText(this, message, length).show()
 }
 
+private const val TAG_URL = "ANNOTATION_TAG_URL"
+
+fun attachLink(
+    source: String, segment: String, link: String
+): AnnotatedString {
+    // Builder to attach metadata(link)
+    val builder = AnnotatedString.Builder()
+    // Load current text into the builder
+    builder.append(source)
+
+    // Get the start of the span "My website"
+    val start = source.indexOf(segment)
+    // Get the end of the span
+    val end = start + segment.length
+    // Create a hyperlink text style
+    val hyperlinkStyle = SpanStyle(
+        color = Color.Blue, textDecoration = TextDecoration.Underline
+    )
+
+    // Style "My website" to make it look like a link
+    builder.addStyle(hyperlinkStyle, start, end)
+    // Attach the link to the span. We can then access it via the TAG_URL
+    builder.addStringAnnotation(TAG_URL, link, start, end)
+
+    return builder.toAnnotatedString()
+}
+
+
+@OptIn(ExperimentalTextApi::class)
 @Preview(showBackground = true)
 @Composable
 fun TextExample() {
     /**
+     * https://github.com/DmytroShuba/Custom-Theme-In-Jetpack-Compose
      * https://github.com/Foso/Jetpack-Compose-Playground/blob/master/app/src/main/java/de/jensklingenberg/jetpackcomposeplayground/mysamples/github/foundation/TextExample.kt
+     * https://stackoverflow.com/questions/69987940/jetpack-compose-custom-badge-with-dynamic-text-size-with-circleshape-is-not-draw
      * https://dmytroshuba.com/blog/complete-guide-to-rich-text-in-jetpack-compose/
      * https://developer.android.com/jetpack/compose/text?hl=vi
+     * https://developer.android.com/jetpack/compose/text/display-text
      * */
+    val layoutResult = remember {
+        mutableStateOf<TextLayoutResult?>(null)
+    }
     val context = LocalContext.current
     val offset = Offset(5.0f, 10.0f)
     Column(modifier = Modifier.padding(10.dp)) {
@@ -114,29 +201,11 @@ fun TextExample() {
         )
         // AnnotatedString/RichText
         Text(
-            text = "Hello World!",
-            style = TextStyle(
-                fontSize = 24.sp,
-                shadow = Shadow(
-                    color = Color.Blue,
-                    offset = offset,
-                    blurRadius = 3f
+            text = "Hello World!", style = TextStyle(
+                fontSize = 24.sp, shadow = Shadow(
+                    color = Color.Blue, offset = offset, blurRadius = 3f
                 )
             )
-        )
-        // TextStyle, SpanStyle, ParagraphStyle
-        Text(
-            buildAnnotatedString {
-                withStyle(style = SpanStyle(color = Color.Blue)) {
-                    append("H")
-                }
-                append("ello ")
-
-                withStyle(style = SpanStyle(color = Color.Red, fontWeight = FontWeight.Bold)) {
-                    append("W")
-                }
-                append("orld")
-            }
         )
         // Layout measure
         // convenience API of creating a custom LayoutModifier modifier, without having to create
@@ -150,31 +219,106 @@ fun TextExample() {
             Spacer(modifier = Modifier.size(10.dp))
             TextMeasure("1000")
         }
-    }
-}
+        // Hyperlink
+        Hyperlink(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            text = "Good URL: https://www.google.com/"
+        )
+        // Clickable text
+        val uriHandler = LocalUriHandler.current
+        val text = "Here is my website"
+        val link = "https://www.google.com/"
+        val annotatedString = attachLink(
+            source = text, segment = "my website", link = link,
+        )
+        ClickableText(text = annotatedString, onClick = {
+            annotatedString.getStringAnnotations(
+                TAG_URL, it, it
+            ).firstOrNull()?.let { url ->
+                uriHandler.openUri(url.item)
+            }
+        })
+        Spacer(modifier = Modifier.size(10.dp))
+        // Display text from resource
+        Text(stringResource(R.string.hello_world))
+        Spacer(modifier = Modifier.size(10.dp))
+        // Gradient text
+        val gradientColors = listOf(Color.Cyan, Color.Gray, Color.Red, Color.Green)
+        Text(
+            text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            style = TextStyle(
+                brush = Brush.linearGradient(
+                    colors = gradientColors
+                )
+            )
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        // TextStyle, SpanStyle
+        Text(buildAnnotatedString {
+            withStyle(style = SpanStyle(color = Color.Blue)) {
+                append("H")
+            }
+            append("ello ")
 
-@Composable
-fun TextMeasure(text: String) {
-    Text(
-        text,
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.error, shape = CircleShape)
-            .badgeLayout(),
-        color = Color.White
-    )
-}
+            withStyle(style = SpanStyle(color = Color.Red, fontWeight = FontWeight.Bold)) {
+                append("W")
+            }
+            append("orld")
+        })
+        Spacer(modifier = Modifier.size(10.dp))
+        // Gradient RichText (Opacity in a span of text)
+        val rainbowColors = listOf(Color.Red, Color.Yellow, Color.Blue, Color.Magenta)
+        Text(text = buildAnnotatedString {
+            append("Do not allow people to dim your shine\n")
+            withStyle(
+                SpanStyle(
+                    brush = Brush.linearGradient(
+                        colors = rainbowColors
+                    ), alpha = .5f
+                )
+            ) {
+                append("because they are blinded")
+            }
+            append("\nTell them to put some sunglasses on. ❤️")
+        })
+        Column(
+            modifier = Modifier
+                .padding(vertical = 10.dp)
+                .background(
+                    color = Color.Gray.copy(alpha = .2f)
+                )
+                .border(
+                    width = 1.dp,
+                    color = Color.Red,
+                    shape = RoundedCornerShape(5.dp),
+                ).padding(10.dp)
+        ) {
+            Text(buildString {
+                repeat(20) {
+                    append("_")
+                    append("|")
+                }
+            }, letterSpacing = 4.sp, onTextLayout = { layoutResult.value = it })
+            Spacer(modifier = Modifier.size(10.dp))
+            // Paragraph (ParagraphStyle)
+            Text(buildAnnotatedString {
+                withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
+                    withStyle(style = SpanStyle(color = Color.Blue)) {
+                        append("Hello\n")
+                    }
+                    withStyle(
+                        style = SpanStyle(
+                            fontWeight = FontWeight.Bold, color = Color.Red
+                        )
+                    ) {
+                        append("World\n")
+                    }
+                    append("Compose")
+                }
+            })
+        }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DojetpackcomposeTheme {
-        Greeting("Android")
     }
 }
