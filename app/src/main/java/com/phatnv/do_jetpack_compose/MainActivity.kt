4f729591b03d546e3d91a3b671b3f52126b5b778
phatnv <phat.nv@themansions.vn>
2023-10-25 09:11:29 +0700

    Update button

--- app/src/main/java/com/phatnv/do_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phatnv/do_jetpack_compose/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -32,6 +33,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedIconButton
@@ -107,6 +109,17 @@ fun ButtonExample() {
         TextButton(onClick = { /*TODO*/ }) {
             Text(text = "Text Button")
         }
+        IconButton(modifier = Modifier.then(Modifier.size(24.dp)), onClick = { /*TODO*/ }) {
+            Icon(
+                Icons.Filled.Search,
+                "contentDescription",
+            )
+        }
+        Icon(
+            Icons.Filled.Search,
+            contentDescription = null,
+            modifier = Modifier.clickable { }
+        )
         // Progress Button (loading)
         var loading by remember {
             mutableStateOf(false)
