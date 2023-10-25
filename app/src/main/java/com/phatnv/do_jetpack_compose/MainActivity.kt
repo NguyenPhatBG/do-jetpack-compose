9ebf6d4d7f1639a10a728b176e06a2a633f2271a
phatnv <phat.nv@themansions.vn>
2023-10-25 15:06:36 +0700

    Add slider with label

--- app/src/main/java/com/phatnv/do_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phatnv/do_jetpack_compose/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -19,8 +24,6 @@ import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,10 +34,12 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.phatnv.do_jetpack_compose.ui.theme.DojetpackcomposeTheme
 import java.util.Locale
@@ -46,8 +51,7 @@ class MainActivity : ComponentActivity() {
             DojetpackcomposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     SliderExample()
                 }
@@ -92,7 +96,7 @@ fun SliderExample() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.Start
     ) {
-        Text("Only ${roundedOnlyNumber.toString()}", color = Color.Black)
+        Text("Only $roundedOnlyNumber", color = Color.Black)
         Slider(
             value = sliderPosition,
             onValueChange = { sliderPosition = it },
@@ -113,8 +117,7 @@ fun SliderExample() {
             style = TextStyle(
                 textDecoration = TextDecoration.combine(
                     listOf(
-                        TextDecoration.Underline,
-                        TextDecoration.LineThrough
+                        TextDecoration.Underline, TextDecoration.LineThrough
                     )
                 ),
                 fontWeight = FontWeight.Bold,
@@ -122,32 +125,99 @@ fun SliderExample() {
             ),
             overflow = TextOverflow.Ellipsis,
         )
-        RangeSlider(
-            value = sliderPositionInRange,
+        RangeSlider(value = sliderPositionInRange,
             steps = 5,
             valueRange = 0f..100f,
             onValueChange = { range -> sliderPositionInRange = range },
             onValueChangeFinished = {
                 Log.d("Finish: ", "$minValue, $maxValue")
-            }
+            })
+        Spacer(modifier = Modifier.size(30.dp))
+        Divider(thickness = 1.dp, color = Color.Black)
+        Spacer(modifier = Modifier.size(30.dp))
+        var sliderPosition by remember { mutableStateOf(15f) }
+        SliderWithLabel(
+            value = sliderPosition,
+            finiteEnd = false,
+            labelMinWidth = 30.dp,
+            valueRange = 0f..100f,
+            onRadiusChange = {
+                sliderPosition = it
+            },
         )
     }
 }
 
 // ============================ End Slider  ============================ //
+// ============================ Start Slider With SliderLabel  ============================ //
+@Composable
+fun SliderWithLabel(
+    value: Float,
+    valueRange: ClosedFloatingPointRange<Float>,
+    finiteEnd: Boolean,
+    labelMinWidth: Dp = 24.dp,
+    onRadiusChange: (Float) -> Unit
+) {
+    Column {
+        BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
+            val offset = getSliderOffset(
+                value = value, valueRange = valueRange, boxWidth = maxWidth,
+                // Since we use a padding of 4.dp on either sides of the SliderLabel
+                // we need to account for this in our calculation
+                labelWidth = labelMinWidth + 8.dp
+            )
+            val endValueText =
+                if (!finiteEnd && value >= valueRange.endInclusive) "${value.toInt()}+" else value.toInt()
+                    .toString()
+
+            SliderLabel(label = valueRange.start.toInt().toString(), minWidth = labelMinWidth)
+
+            if (value > valueRange.start) {
+                SliderLabel(
+                    label = endValueText,
+                    minWidth = labelMinWidth,
+                    modifier = Modifier.padding(start = offset)
+                )
+            }
+        }
+        Slider(
+            enabled = true,
+            steps = 5,
+            value = value,
+            onValueChange = {
+                onRadiusChange(it)
+            },
+            valueRange = valueRange,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun SliderLabel(label: String, minWidth: Dp, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        label, textAlign = TextAlign.Center, color = Color.White,
         modifier = modifier
+            .background(
+                color = MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(4.dp)
+            )
+            .padding(4.dp)
+            .defaultMinSize(minWidth = minWidth),
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DojetpackcomposeTheme {
-        Greeting("Android")
-    }
+private fun getSliderOffset(
+    value: Float,
+    valueRange: ClosedFloatingPointRange<Float>,
+    boxWidth: Dp,
+    labelWidth: Dp,
+): Dp {
+    val coerced = value.coerceIn(valueRange.start, valueRange.endInclusive)
+    val positionFraction = calcFraction(valueRange.start, valueRange.endInclusive, coerced)
+    return (boxWidth - labelWidth) * positionFraction
 }
+
+// Calculate the 0..1 fraction that "pos" value presents between "a" and "b"
+private fun calcFraction(a: Float, b: Float, pos: Float) =
+    (if (b - a == 0f) 0f else (pos - a) / (b - a)).coerceIn(0f, 1f)
+// ============================ End Slider With SliderLabel  ============================ //
